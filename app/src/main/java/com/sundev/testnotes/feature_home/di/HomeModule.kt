c3be30c76a0282c71af0ac2c6cc1b9f425dd1cd3
smaharjan974 <sanjaymaharjan.yunige@gmail.com>
2025-03-11 16:45:53 +0900

    implemented test case for add, delete and get notes

--- app/src/main/java/com/sundev/testnotes/feature_home/di/HomeModule.kt
+++ app/src/main/java/com/sundev/testnotes/feature_home/di/HomeModule.kt
@@ -1,18 +1,12 @@
 package com.sundev.testnotes.feature_home.di
 
-import android.content.Context
-import androidx.room.Room
-import com.sundev.testnotes.NotesApp
-import com.sundev.testnotes.core.data.local.AppDatabase
-import com.sundev.testnotes.core.data.repository.NotesRepositoryImpl
 import com.sundev.testnotes.core.domain.repository.NotesRepository
 import com.sundev.testnotes.feature_home.domain.GetNotesUseCase
-import com.sundev.testnotes.feature_home.domain.ListenNotesUseCases
+import com.sundev.testnotes.core.domain.ListenNotesUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 @Module
 @InstallIn(ViewModelComponent::class)
