cf3d925b52bcce385301fb31ec19c915f1a21b1e
Sanjay Maharjan <smaharjan974@gmail.com>
2025-03-11 16:52:46 +0900

    Revert "Revert "implemented test case for add, delete and get notes""

--- app/src/main/java/com/sundev/testnotes/feature_addNote/domain/DeleteNoteUseCase.kt
+++ app/src/main/java/com/sundev/testnotes/feature_addNote/domain/DeleteNoteUseCase.kt
@@ -2,16 +2,18 @@ package com.sundev.testnotes.feature_addNote.domain
 
 import com.sundev.testnotes.core.data.repository.NotesRepositoryImpl
 import com.sundev.testnotes.core.domain.repository.NotesRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class DeleteNoteUseCase(
-    private val repository: NotesRepository
+    private val repository: NotesRepository,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 
 ) {
 
     suspend fun execute(id: Int) {
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             repository.delete(id)
         }
     }
